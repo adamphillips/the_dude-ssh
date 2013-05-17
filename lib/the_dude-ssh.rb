76f1a6e8a8a1d76da4014cf47a0e53722263230d
Adam Phillips <aphillips@scholastic.co.uk>
2013-05-17 22:09:14 +0100

    Added a simple command
    Added a simple SSH ls command to check everything is working

--- lib/the_dude-ssh.rb
+++ lib/the_dude-ssh.rb
@@ -1,3 +1,13 @@
 require 'the_dude'
 require 'net/ssh'
 
+TheDude::Dsl.run do
+  var :path, /[\S|\\\s]+/
+  var :server, /\S+/
+
+  command 'ls :path on :server' do |path, server|
+    Net::SSH.start server, nil do |ssh|
+      ssh.exec! "cd #{path} && ls -la"
+    end
+  end
+end
